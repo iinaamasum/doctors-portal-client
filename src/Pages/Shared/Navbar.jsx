dca298f853e90472296c31c28509cee53a48cc3b
iinaamasum <iinaamasum@gmail.com>
2022-05-10 20:06:03 +0600

    care section image fix with navbar customization

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/icons/logo.png';
 
 const Navbar = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(true);
   const navLinks = [
     { id: 1, path: '/', name: 'Home' },
     { id: 2, path: '/appointment', name: 'Appointment' },
@@ -13,6 +13,8 @@ const Navbar = () => {
     { id: 4, path: '/contact', name: 'Contact Us' },
     { id: 5, path: '/about', name: 'About' },
   ];
+
+  console.log(drawerOpen);
   return (
     <section
       style={{ maxWidth: '1024px' }}
@@ -21,7 +23,7 @@ const Navbar = () => {
       <div className="navbar bg-gradient-to-r from-[#19D3AE] to-[#0FCFEC] rounded-full text-gray-100 flex">
         <div className="flex justify-between items-center w-full mx-3">
           <div
-            className={`drawer ml-0 -mt-1 rounded-r lg:hidden absolute left-0 top-0 bg-accent ${
+            className={`drawer ml-0 -mt-1 rounded-r lg:hidden absolute left-0 top-0 bg-accent z-50 ${
               drawerOpen ? 'w-1/2' : 'hidden'
             } `}
           >
@@ -47,16 +49,20 @@ const Navbar = () => {
             <span className="text-accent">Dentist Portal</span>
           </Link>
 
-          <div onClick={() => setDrawerOpen(!drawerOpen)}>
-            <label
-              tabIndex="0"
-              className="btn btn-ghost lg:hidden font-bold text-accent"
-            >
-              {drawerOpen ? (
-                <MdCloseFullscreen size={30} />
-              ) : (
-                <AiOutlineMenuUnfold size={30} />
-              )}
+          <div className="lg:hidden">
+            <label class="btn btn-circle swap swap-rotate">
+              <input type="checkbox" />
+
+              <MdCloseFullscreen
+                onClick={() => setDrawerOpen(!drawerOpen)}
+                class="swap-off fill-current"
+                size={30}
+              />
+              <AiOutlineMenuUnfold
+                onClick={() => setDrawerOpen(!drawerOpen)}
+                class="swap-on fill-current"
+                size={30}
+              />
             </label>
           </div>
         </div>
