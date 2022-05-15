98589e5e4f988adf89e1daa06ce41189bdbfd146
iinaamasum <iinaamasum@gmail.com>
2022-05-16 01:49:09 +0600

    login page structure done

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import loginImg from '../../../assets/images/login.png';
+import Navbar from '../../Shared/Navbar';
+import Social from '../Social/Social';
 
 const Login = () => {
   const {
@@ -10,15 +14,121 @@ const Login = () => {
   const onSubmit = (data) => console.log(data);
   return (
     <section>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('firstName', { required: true })} />
-        {errors.firstName?.type === 'required' && 'First name is required'}
+      <Navbar />
 
-        <input {...register('lastName', { required: true })} />
-        {errors.lastName && 'Last name is required'}
+      <div
+        style={{ maxWidth: '1024px' }}
+        className="container mx-auto px-4 my-10 md:my-16"
+      >
+        <div className="md:flex md:flex-row-reverse">
+          <div className="w-full md:w-1/2 hidden md:block">
+            <img src={loginImg} alt="" />
+          </div>
 
-        <input type="submit" />
-      </form>
+          <div class="card w-full md:w-1/2 bg-base-100 shadow-lg mr-2">
+            <div class="card-body mt-6">
+              <h2 class="card-title mx-auto text-4xl md:my-5">LogIn</h2>
+              <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
+                <div class="form-control w-full">
+                  <label class="">
+                    <span class="text-secondary font-semibold text-lg">
+                      Email
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="example@email.com"
+                    class="input input-bordered w-full"
+                    {...register('email', {
+                      required: {
+                        value: true,
+                        message: 'Email Required !!!',
+                      },
+                      pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+                        message: 'Invalid Email Provided !!!',
+                      },
+                    })}
+                  />
+                  <label class="level font-bold">
+                    {errors.email?.type === 'required' && (
+                      <span className="label-text-alt text-red-500">
+                        {errors.email.message}
+                      </span>
+                    )}
+                    {errors.email?.type === 'pattern' && (
+                      <span className="label-text-alt text-red-500">
+                        {errors.email.message}
+                      </span>
+                    )}
+                  </label>
+                </div>
+                <div class="form-control w-full">
+                  <label class="">
+                    <span class="text-secondary font-semibold text-lg">
+                      Password
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="abcd123$"
+                    class="input input-bordered w-full"
+                    {...register('password', {
+                      required: {
+                        value: true,
+                        message: 'Password Required !!!',
+                      },
+                      pattern: {
+                        value: /(?=.*[!#$%&?^*@~() "])(?=.{8,})/,
+                        message:
+                          'Password Must be 8 char including a special char !!!',
+                      },
+                    })}
+                  />
+                  <label class="level font-bold">
+                    {errors.password?.type === 'required' && (
+                      <span className="label-text-alt text-red-500">
+                        {errors.password.message}
+                      </span>
+                    )}
+                    {errors.password?.type === 'pattern' && (
+                      <span className="label-text-alt text-red-500">
+                        {errors.password.message}
+                      </span>
+                    )}
+                  </label>
+                </div>
+                <p className="mt-10">
+                  Don't have an account?{' '}
+                  <Link
+                    className="btn-link text-purple-500 font-semibold"
+                    to="/signup"
+                  >
+                    Create Account Now
+                  </Link>
+                </p>
+                <p>
+                  Forgot Password?{' '}
+                  <Link
+                    className="btn-link text-purple-500 font-semibold"
+                    to="/signup"
+                  >
+                    Click Here To Reset
+                  </Link>
+                </p>
+
+                <input
+                  className="btn my-3 w-full tracking-wider capitalize text-xl"
+                  value="LogIn"
+                  type="submit"
+                />
+                <div class="divider">Social LogIn</div>
+                <Social />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/Pages/Auth/Social/Social.jsx
+++ src/Pages/Auth/Social/Social.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BsFacebook, BsGithub, BsGoogle } from 'react-icons/bs';
 
 const Social = () => {
-  return <div>Social</div>;
+  return (
+    <div className="text-center">
+      <button class="mr-2 hover:text-slate-500">
+        <BsGoogle size={30} />
+      </button>
+      <button class="mr-2 hover:text-slate-500">
+        <BsFacebook size={30} />
+      </button>
+      <button class=" hover:text-slate-500">
+        <BsGithub size={30} />
+      </button>
+    </div>
+  );
 };
 
 export default Social;
